a58b1344489a3bf75fe8d5d8e34e0b9c7e4b1006
viti95 <victornt95@gmail.com>
2020-05-05 17:50:10 +0200

    Removed left forgotten debug check

--- am_map.c
+++ am_map.c
@@ -952,13 +952,6 @@ void AM_drawFline(fline_t *fl,
 
 	static fuck = 0;
 
-	// For debugging only
-	if (fl->a.x < 0 || fl->a.x >= f_w || fl->a.y < 0 || fl->a.y >= f_h || fl->b.x < 0 || fl->b.x >= f_w || fl->b.y < 0 || fl->b.y >= f_h)
-	{
-		fprintf(stderr, "fuck %d \r", fuck++);
-		return;
-	}
-
 #define PUTDOT(xx, yy, cc) fb[(yy)*f_w + (xx)] = (cc)
 
 	dx = fl->b.x - fl->a.x;
