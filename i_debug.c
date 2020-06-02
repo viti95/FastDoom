9a6bf0fa42b146a24bff7fd2277610bd7667b41e
viti95 <victornt95@gmail.com>
2020-06-02 22:35:41 +0200

    Implemented more debugging functions

--- i_debug.c
+++ i_debug.c
@@ -1,16 +1,16 @@
+#include <stdio.h>
+#include <string.h>
 #include "doomtype.h"
 
-void I_DebugWrite(int x, int y, char *message)
-{
-    int i = 0;
+void I_DebugClearLine(){
+    int i;
+
     byte *bwscreen = (byte *)0xB0000;
 
-    while (*message)
+    for (i = 0; i < 80; i++)
     {
-        bwscreen[y * 160 + x * 2 + i * 2] = *(bwscreen);
-        bwscreen[y * 160 + x * 2 + i * 2 + 1] = 7;
-        message++;
-        i++;
+        bwscreen[i * 2] = ' ';
+        bwscreen[i * 2 + 1] = 7;
     }
 }
 
@@ -25,4 +25,49 @@ void I_DebugClear()
         bwscreen[i * 2] = ' ';
         bwscreen[i * 2 + 1] = 7;
     }
+}
+
+void I_DebugWriteString(int x, int y, char *message)
+{
+    int i = 0;
+    byte *bwscreen = (byte *)0xB0000;
+
+    while (*message)
+    {
+        bwscreen[y * 160 + x * 2 + i * 2] = *(message);
+        bwscreen[y * 160 + x * 2 + i * 2 + 1] = 7;
+        message++;
+        i++;
+    }
+}
+
+void I_DebugWriteInteger(int x, int y, int value)
+{
+    char message[11];
+    sprintf(message, "%ld", value);
+    I_DebugWriteString(x, y, message);
+}
+
+void I_DebugWriteLineString(char *message)
+{
+    int i = 0;
+    byte *bwscreen = (byte *)0xB0000;
+
+    _fmemcpy(bwscreen + 2 * 80, bwscreen, 2 * 80 * 24);
+    I_DebugClearLine();
+
+    while (*message)
+    {
+        bwscreen[i * 2] = *(message);
+        bwscreen[i * 2 + 1] = 7;
+        message++;
+        i++;
+    }
+}
+
+void I_DebugWriteLineInteger(int value)
+{
+    char message[11];
+    sprintf(message, "%ld", value);
+    I_DebugWriteLineString(message);
 }
--- i_debug.h
+++ i_debug.h
@@ -1,2 +1,6 @@
-void I_DebugWrite(int x, int y, char *message);
+void I_DebugWriteString(int x, int y, char *message);
+void I_DebugWriteInteger(int x, int y, int value);
+void I_DebugWriteLineString(char *message);
+void I_DebugWriteLineInteger(int value);
 void I_DebugClear(void);
+void I_DebugClearLine(void);
