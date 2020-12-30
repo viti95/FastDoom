viti95/FastDoom

commit a9ab26affab849e7e3e155caa1a6410c96ab60c1

Optimize automap routines. Optimize screen buffers declarations (now they are static). Small optimizations for I_UpdateBox.

am_map.c
@@ -110,8 +110,8 @@
 #define FTOM(x) FixedMul(((x) << 16), scale_ftom)
 #define MTOF(x) (FixedMul((x), scale_mtof) >> 16)
 // translates between frame-buffer and map coordinates
-#define CXMTOF(x) (f_x + MTOF((x)-m_x))
-#define CYMTOF(y) (f_y + (f_h - MTOF((y)-m_y)))
+#define CXMTOF(x) (MTOF((x)-m_x))
+#define CYMTOF(y) ((SCREENHEIGHT - 32 - MTOF((y)-m_y)))
 
 // the following is crap
 #define LINE_NEVERSEE ML_DONTDRAW
@@ -201,19 +201,8 @@ static int grid = 0;
 static int leveljuststarted = 1; // kluge until AM_LevelInit() is called
 
 boolean automapactive = false;
-static int finit_width = SCREENWIDTH;
-static int finit_height = SCREENHEIGHT - 32;
-
-// location of window on screen
-static int f_x;
-static int f_y;
-
-// size of window on screen
-static int f_w;
-static int f_h;
 
 static int lightlev; // used for funky strobing effect
-static byte *fb;	 // pseudo-frame buffer
 static int amclock;
 
 static mpoint_t m_paninc;	 // how far the window pans each tic (map coords)
@@ -257,7 +246,7 @@ static fixed_t scale_ftom;
 
 static player_t *plr; // the player represented by an arrow
 
-static patch_t *marknums[10];				  // numbers used for marking by the automap
+static patch_t *marknums[AM_NUMMARKPOINTS];	  // numbers used for marking by the automap
 static mpoint_t markpoints[AM_NUMMARKPOINTS]; // where the points are
 static int markpointnum = 0;				  // next point to be assigned
 
@@ -277,8 +266,8 @@ void AM_activateNewScale(void)
 {
 	m_x += m_w / 2;
 	m_y += m_h / 2;
-	m_w = FTOM(f_w);
-	m_h = FTOM(f_h);
+	m_w = FTOM(SCREENWIDTH);
+	m_h = FTOM(SCREENHEIGHT - 32);
 	m_x -= m_w / 2;
 	m_y -= m_h / 2;
 	m_x2 = m_x + m_w;
@@ -318,7 +307,7 @@ void AM_restoreScaleAndLoc(void)
 	m_y2 = m_y + m_h;
 
 	// Change the scaling multipliers
-	scale_mtof = FixedDiv(f_w << FRACBITS, m_w);
+	scale_mtof = FixedDiv(SCREENWIDTH << FRACBITS, m_w);
 	scale_ftom = FixedDiv(FRACUNIT, scale_mtof);
 }
 
@@ -361,12 +350,13 @@ void AM_findMinMaxBoundaries(void)
 	max_w = max_x - min_x;
 	max_h = max_y - min_y;
 
-	a = FixedDiv(f_w << FRACBITS, max_w);
-	b = FixedDiv(f_h << FRACBITS, max_h);
+	a = FixedDiv(SCREENWIDTH << FRACBITS, max_w);
+	b = FixedDiv((SCREENHEIGHT - 32) << FRACBITS, max_h);
 
-	// OPTIMIZE MIN/MAX
 	min_scale_mtof = a < b ? a : b;
-	max_scale_mtof = FixedDiv(f_h << FRACBITS, 2 * PLAYERRADIUS);
+
+	// OPTIMIZE MAX STATIC VALUE
+	max_scale_mtof = FixedDiv((SCREENHEIGHT - 32) << FRACBITS, 2 * PLAYERRADIUS);
 }
 
 //
@@ -405,7 +395,6 @@ void AM_initVariables(void)
 	static event_t st_notify = {ev_keyup, AM_MSGENTERED};
 
 	automapactive = true;
-	fb = screens[0];
 
 	f_oldloc.x = MAXINT;
 	amclock = 0;
@@ -415,8 +404,8 @@ void AM_initVariables(void)
 	ftom_zoommul = FRACUNIT;
 	mtof_zoommul = FRACUNIT;
 
-	m_w = FTOM(f_w);
-	m_h = FTOM(f_h);
+	m_w = FTOM(SCREENWIDTH);
+	m_h = FTOM(SCREENHEIGHT - 32);
 
 	plr = &players;
 	m_x = plr->mo->x - m_w / 2;
@@ -472,10 +461,6 @@ void AM_clearMarks(void)
 void AM_LevelInit(void)
 {
 	leveljuststarted = 0;
-
-	f_x = f_y = 0;
-	f_w = finit_width;
-	f_h = finit_height;
 
 	AM_clearMarks();
 
@@ -739,14 +724,6 @@ void AM_Ticker(void)
 }
 
 //
-// Clear automap frame buffer.
-//
-void AM_clearFB(int color)
-{
-	memset(fb, color, f_w * f_h);
-}
-
-//
 // Automap clipping of lines.
 //
 // Based on Cohen-Sutherland clipping algorithm but with a slightly
@@ -777,11 +754,11 @@ AM_clipMline(mline_t *ml,
 	(oc) = 0;                 \
 	if ((my) < 0)             \
 		(oc) |= TOP;          \
-	else if ((my) >= f_h)     \
+	else if ((my) >= (SCREENHEIGHT - 32))     \
 		(oc) |= BOTTOM;       \
 	if ((mx) < 0)             \
 		(oc) |= LEFT;         \
-	else if ((mx) >= f_w)     \
+	else if ((mx) >= SCREENWIDTH)     \
 		(oc) |= RIGHT;
 
 	// do trivial rejects and outcodes
@@ -844,15 +821,15 @@ AM_clipMline(mline_t *ml,
 		{
 			dy = fl->a.y - fl->b.y;
 			dx = fl->b.x - fl->a.x;
-			tmp.x = fl->a.x + (dx * (fl->a.y - f_h)) / dy;
-			tmp.y = f_h - 1;
+			tmp.x = fl->a.x + (dx * (fl->a.y - (SCREENHEIGHT - 32))) / dy;
+			tmp.y = SCREENHEIGHT - 32 - 1;
 		}
 		else if (outside & RIGHT)
 		{
 			dy = fl->b.y - fl->a.y;
 			dx = fl->b.x - fl->a.x;
-			tmp.y = fl->a.y + (dy * (f_w - 1 - fl->a.x)) / dx;
-			tmp.x = f_w - 1;
+			tmp.y = fl->a.y + (dy * (SCREENWIDTH - 1 - fl->a.x)) / dx;
+			tmp.x = SCREENWIDTH - 1;
 		}
 		else if (outside & LEFT)
 		{
@@ -897,7 +874,7 @@ void AM_drawFline(fline_t *fl,
 	register int ay;
 	register int d;
 
-#define PUTDOT(xx, yy, cc) fb[(yy)*f_w + (xx)] = (cc)
+#define PUTDOT(xx, yy, cc) screen0[Mul320(yy) + (xx)] = (cc)
 
 	dx = fl->b.x - fl->a.x;
 	// OPTIMIZE NEGATE
@@ -1128,7 +1105,6 @@ void AM_drawPlayers(void)
 {
 	int i;
 	player_t *p;
-	static int their_colors[] = {GREENS, GRAYS, BROWNS, REDS};
 	int color;
 
 	if (cheating)
@@ -1144,7 +1120,7 @@ void AM_drawPlayers(void)
 	if (p->powers[pw_invisibility])
 		color = 246; // *close* to black
 	else
-		color = their_colors[0];
+		color = GREENS;
 
 	AM_drawLineCharacter(player_arrow, NUMPLYRLINES, 0, p->mo->angle,
 							color, p->mo->x, p->mo->y);
@@ -1170,27 +1146,19 @@ void AM_drawThings(int colors,
 
 void AM_drawMarks(void)
 {
-	int i, fx, fy, w, h;
+	int i;
 
 	for (i = 0; i < AM_NUMMARKPOINTS; i++)
 	{
 		if (markpoints[i].x != -1)
 		{
-			//      w = SHORT(marknums[i]->width);
-			//      h = SHORT(marknums[i]->height);
-			w = 5; // because something's wrong with the wad, i guess
-			h = 6; // because something's wrong with the wad, i guess
+			int fx, fy;
 			fx = CXMTOF(markpoints[i].x);
 			fy = CYMTOF(markpoints[i].y);
-			if (fx >= f_x && fx <= f_w - w && fy >= f_y && fy <= f_h - h)
-				V_DrawPatch(fx, fy, FB, marknums[i]);
+			if (fx >= 0 && fy >= 0 && fy <= (SCREENHEIGHT - 32) - 6)
+				V_DrawPatch(0, 0, screen0, marknums[i]);
 		}
 	}
-}
-
-void AM_drawCrosshair(int color)
-{
-	fb[(f_w * (f_h + 1)) / 2] = color; // single point for now
 }
 
 void AM_Drawer(void)
@@ -1198,16 +1166,15 @@ void AM_Drawer(void)
 	if (!automapactive)
 		return;
 
-	AM_clearFB(BACKGROUND);
+	SetDWords(screen0, BACKGROUND, (SCREENWIDTH * (SCREENHEIGHT - 32)) / 4); // Clear automap frame buffer
 	if (grid)
 		AM_drawGrid(GRIDCOLORS);
 	AM_drawWalls();
 	AM_drawPlayers();
 	if (cheating == 2)
 		AM_drawThings(THINGCOLORS, THINGRANGE);
-	//AM_drawCrosshair(XHAIRCOLORS);
 
 	AM_drawMarks();
 
-	V_MarkRect(f_x, f_y, f_w, f_h);
+	V_MarkRect(0, 0, SCREENWIDTH, SCREENHEIGHT - 32);
 }

d_main.c
@@ -406,7 +406,7 @@ void D_PageTicker(void)
 //
 void D_PageDrawer(void)
 {
-    V_DrawPatch(0, 0, 0, W_CacheLumpName(pagename, PU_CACHE));
+    V_DrawPatch(0, 0, screen0, W_CacheLumpName(pagename, PU_CACHE));
 }
 
 //
@@ -930,10 +930,6 @@ void D_DoomMain(void)
         }
         autostart = true;
     }
-
-    // init subsystems
-    printf("V_Init: allocate screens.\n");
-    V_Init();
 
     printf("M_LoadDefaults: Load system defaults.\n");
     M_LoadDefaults(); // load before initing other systems

f_finale.c
@@ -334,7 +334,7 @@ void F_TextWrite(void)
 
 	// erase the entire screen to a tiled background
 	src = W_CacheLumpName(finaleflat, PU_CACHE);
-	dest = screens[0];
+	dest = screen0;
 
 	for (y = 0; y < SCREENHEIGHT; y++)
 	{
@@ -378,7 +378,7 @@ void F_TextWrite(void)
 		w = SHORT(hu_font[c]->width);
 		if (cx + w > SCREENWIDTH)
 			break;
-		V_DrawPatch(cx, cy, 0, hu_font[c]);
+		V_DrawPatch(cx, cy, screen0, hu_font[c]);
 		cx += w;
 	}
 }
@@ -650,7 +650,7 @@ void F_CastPrint(char *text)
 		}
 
 		w = SHORT(hu_font[c]->width);
-		V_DrawPatch(cx, 180, 0, hu_font[c]);
+		V_DrawPatch(cx, 180, screen0, hu_font[c]);
 		cx += w;
 	}
 }
@@ -658,7 +658,7 @@ void F_CastPrint(char *text)
 //
 // F_CastDrawer
 //
-void V_DrawPatchFlipped(int x, int y, int scrn, patch_t *patch);
+void V_DrawPatchFlipped(int x, int y, byte *scrn, patch_t *patch);
 
 void F_CastDrawer(void)
 {
@@ -669,7 +669,7 @@ void F_CastDrawer(void)
 	patch_t *patch;
 
 	// erase the entire screen to a background
-	V_DrawPatch(0, 0, 0, W_CacheLumpName("BOSSBACK", PU_CACHE));
+	V_DrawPatch(0, 0, screen0, W_CacheLumpName("BOSSBACK", PU_CACHE));
 
 	F_CastPrint(castorder[castnum].name);
 
@@ -681,9 +681,9 @@ void F_CastDrawer(void)
 
 	patch = W_CacheLumpNum(lump + firstspritelump, PU_CACHE);
 	if (flip)
-		V_DrawPatchFlipped(160, 170, 0, patch);
+		V_DrawPatchFlipped(160, 170, screen0, patch);
 	else
-		V_DrawPatch(160, 170, 0, patch);
+		V_DrawPatch(160, 170, screen0, patch);
 }
 
 //
@@ -700,7 +700,7 @@ void F_DrawPatchCol(int x,
 	int count;
 
 	column = (column_t *)((byte *)patch + LONG(patch->columnofs[col]));
-	desttop = screens[0] + x;
+	desttop = screen0 + x;
 
 	// step through the posts in a column
 	while (column->topdelta != 0xff)
@@ -755,7 +755,7 @@ void F_BunnyScroll(void)
 	if (finalecount < 1180)
 	{
 		V_DrawPatch((SCREENWIDTH - 13 * 8) / 2,
-					(SCREENHEIGHT - 8 * 8) / 2, 0, W_CacheLumpName("END0", PU_CACHE));
+					(SCREENHEIGHT - 8 * 8) / 2, screen0, W_CacheLumpName("END0", PU_CACHE));
 		laststage = 0;
 		return;
 	}
@@ -770,7 +770,7 @@ void F_BunnyScroll(void)
 	}
 
 	sprintf(name, "END%i", stage);
-	V_DrawPatch((SCREENWIDTH - 13 * 8) / 2, (SCREENHEIGHT - 8 * 8) / 2, 0, W_CacheLumpName(name, PU_CACHE));
+	V_DrawPatch((SCREENWIDTH - 13 * 8) / 2, (SCREENHEIGHT - 8 * 8) / 2, screen0, W_CacheLumpName(name, PU_CACHE));
 }
 
 //
@@ -792,23 +792,23 @@ void F_Drawer(void)
 		{
 		case 1:
 #if (EXE_VERSION < EXE_VERSION_ULTIMATE)
-			V_DrawPatch(0, 0, 0,
+			V_DrawPatch(0, 0, screen0,
 						W_CacheLumpName("HELP2", PU_CACHE));
 			break;
 #else
-			V_DrawPatch(0, 0, 0,
+			V_DrawPatch(0, 0, screen0,
 						W_CacheLumpName("CREDIT", PU_CACHE));
 			break;
 #endif
 		case 2:
-			V_DrawPatch(0, 0, 0,
+			V_DrawPatch(0, 0, screen0,
 						W_CacheLumpName("VICTORY2", PU_CACHE));
 			break;
 		case 3:
 			F_BunnyScroll();
 			break;
 		case 4:
-			V_DrawPatch(0, 0, 0,
+			V_DrawPatch(0, 0, screen0,
 						W_CacheLumpName("ENDPIC", PU_CACHE));
 			break;
 		}

f_wipe.c
@@ -166,16 +166,16 @@ int wipe_exitMelt()
 
 int wipe_StartScreen()
 {
-    wipe_scr_start = screens[2];
+    wipe_scr_start = screen2;
     I_ReadScreen(wipe_scr_start);
     return 0;
 }
 
 int wipe_EndScreen()
 {
-    wipe_scr_end = screens[3];
+    wipe_scr_end = screen3;
     I_ReadScreen(wipe_scr_end);
-    V_DrawBlock(0, 0, 0, SCREENWIDTH, SCREENHEIGHT, wipe_scr_start); // restore start scr.
+    V_DrawBlock(0, 0, screen0, SCREENWIDTH, SCREENHEIGHT, wipe_scr_start); // restore start scr.
     return 0;
 }
 
@@ -187,7 +187,7 @@ int wipe_ScreenWipe(int ticks)
     if (!go)
     {
         go = 1;
-        wipe_scr = screens[0];
+        wipe_scr = screen0;
         wipe_initMelt();
     }
 

g_game.c
@@ -875,7 +875,7 @@ void G_DoSaveGame(void)
     sprintf(name, SAVEGAMENAME "%d.dsg", savegameslot);
     description = savedescription;
 
-    save_p = savebuffer = screens[1] + 0x4000;
+    save_p = savebuffer = screen1 + 0x4000;
 
     memcpy(save_p, description, SAVESTRINGSIZE);
     save_p += SAVESTRINGSIZE;

i_ibm.c
@@ -251,15 +251,15 @@ void I_UpdateBox(int x, int y, int w, int h)
     int step;
     byte *dest, *source;
 
+    outp(SC_INDEX, SC_MAPMASK);
+
     sp_x1 = x / 8;
     sp_x2 = (x + w) / 8;
     count = sp_x2 - sp_x1 + 1;
-    offset = Mul320(y) + sp_x1 * 8;
     step = SCREENWIDTH - count * 8;
+    offset = Mul320(y) + sp_x1 * 8;
     poffset = offset / 4;
     pstep = step / 4;
-
-    outp(SC_INDEX, SC_MAPMASK);
 
     if (!(count & 1))
     {
@@ -268,7 +268,7 @@ void I_UpdateBox(int x, int y, int w, int h)
         for (i = 0; i < 4; i++)
         {
             outp(SC_INDEX + 1, 1 << i);
-            source = &screens[0][offset + i];
+            source = &screen0[offset + i];
             dest = destscreen + poffset;
 
             for (j = 0; j < h; j++)
@@ -294,7 +294,7 @@ void I_UpdateBox(int x, int y, int w, int h)
         for (i = 0; i < 4; i++)
         {
             outp(SC_INDEX + 1, 1 << i);
-            source = &screens[0][offset + i];
+            source = &screen0[offset + i];
             dest = destscreen + poffset;
 
             for (j = 0; j < h; j++)
@@ -322,7 +322,7 @@ int olddb[2][4];
 void I_UpdateNoBlit(void)
 {
     int realdr[4];
-    int x, y, w, h;
+    
     // Set current screen
     currentscreen = destscreen;
 
@@ -385,10 +385,14 @@ void I_UpdateNoBlit(void)
     // Update screen
     if (realdr[BOXBOTTOM] <= realdr[BOXTOP])
     {
+        int x, y, w, h;
+
         x = realdr[BOXLEFT];
+        w = realdr[BOXRIGHT] - x + 1;
+
         y = realdr[BOXBOTTOM];
-        w = realdr[BOXRIGHT] - realdr[BOXLEFT] + 1;
-        h = realdr[BOXTOP] - realdr[BOXBOTTOM] + 1;
+        h = realdr[BOXTOP] - y + 1;
+        
         I_UpdateBox(x, y, w, h);
     }
     // Clear box

m_menu.c
@@ -1095,8 +1095,7 @@ void M_QuitDOOM(int choice)
     }
     else
     {
-        sprintf(endstring, "%s\n\n" DOSY,
-                endmsg[(gametic >> 2) % NUM_QUITMESSAGES]);
+        sprintf(endstring, "%s\n\n" DOSY, endmsg[(gametic >> 2) % NUM_QUITMESSAGES]);
     }
 
     M_StartMessage(endstring, M_QuitResponse, true);

r_draw.c
@@ -870,7 +870,7 @@ void R_FillBackScreen(void)
         name = name1;
 
     src = W_CacheLumpName(name, PU_CACHE);
-    dest = screens[1];
+    dest = screen1;
 
     for (y = 0; y < SCREENHEIGHT - SBARHEIGHT; y++)
     {
@@ -885,39 +885,39 @@ void R_FillBackScreen(void)
     patch = W_CacheLumpName("BRDR_T", PU_CACHE);
 
     for (x = 0; x < scaledviewwidth; x += 8)
-        V_DrawPatch(viewwindowx + x, viewwindowy - 8, 1, patch);
+        V_DrawPatch(viewwindowx + x, viewwindowy - 8, screen1, patch);
     patch = W_CacheLumpName("BRDR_B", PU_CACHE);
 
     for (x = 0; x < scaledviewwidth; x += 8)
-        V_DrawPatch(viewwindowx + x, viewwindowy + viewheight, 1, patch);
+        V_DrawPatch(viewwindowx + x, viewwindowy + viewheight, screen1, patch);
     patch = W_CacheLumpName("BRDR_L", PU_CACHE);
 
     for (y = 0; y < viewheight; y += 8)
-        V_DrawPatch(viewwindowx - 8, viewwindowy + y, 1, patch);
+        V_DrawPatch(viewwindowx - 8, viewwindowy + y, screen1, patch);
     patch = W_CacheLumpName("BRDR_R", PU_CACHE);
 
     for (y = 0; y < viewheight; y += 8)
-        V_DrawPatch(viewwindowx + scaledviewwidth, viewwindowy + y, 1, patch);
+        V_DrawPatch(viewwindowx + scaledviewwidth, viewwindowy + y, screen1, patch);
 
     // Draw beveled edge.
     V_DrawPatch(viewwindowx - 8,
                 viewwindowy - 8,
-                1,
+                screen1,
                 W_CacheLumpName("BRDR_TL", PU_CACHE));
 
     V_DrawPatch(viewwindowx + scaledviewwidth,
                 viewwindowy - 8,
-                1,
+                screen1,
                 W_CacheLumpName("BRDR_TR", PU_CACHE));
 
     V_DrawPatch(viewwindowx - 8,
                 viewwindowy + viewheight,
-                1,
+                screen1,
                 W_CacheLumpName("BRDR_BL", PU_CACHE));
 
     V_DrawPatch(viewwindowx + scaledviewwidth,
                 viewwindowy + viewheight,
-                1,
+                screen1,
                 W_CacheLumpName("BRDR_BR", PU_CACHE));
 
     for (i = 0; i < 4; i++)
@@ -926,7 +926,7 @@ void R_FillBackScreen(void)
         outp(SC_INDEX + 1, 1 << i);
 
         dest = (byte *)0xac000;
-        src = screens[1] + i;
+        src = screen1 + i;
         do
         {
             *dest++ = *src;

commit 703fb52ca23010c9451c372d9d9c5e3ff87a980e

Fix automap markings (broken in a9ab26affab849e7e3e155caa1a6410c96ab60c1)

am_map.c
@@ -1111,8 +1111,8 @@ void AM_drawMarks(void)
 			int fx, fy;
 			fx = CXMTOF(markpoints[i].x);
 			fy = CYMTOF(markpoints[i].y);
-			if (fx >= 0 && fy >= 0 && fy <= (SCREENHEIGHT - 32) - 6)
-				V_DrawPatch(0, 0, screen0, marknums[i]);
+            if (fx >= 0 && fx <= SCREENWIDTH - 5 && fy >= 0 && fy <= SCREENHEIGHT - 32 - 6)
+                V_DrawPatch(fx, fy, screen0, marknums[i]);
 		}
 	}
 }