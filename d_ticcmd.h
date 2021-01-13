599e31a1ead9057d674bb24abf43a52c07070330
viti95 <victornt95@gmail.com>
2021-01-13 15:40:05 +0100

    Remove ticcmd_t unused variables

--- d_ticcmd.h
+++ d_ticcmd.h
@@ -30,8 +30,6 @@ typedef struct
     char forwardmove;  // *2048 for move
     char sidemove;     // *2048 for move
     short angleturn;   // <<16 for angle delta
-    short consistancy; // checks for net game
-    byte chatchar;
     byte buttons;
 } ticcmd_t;
 
