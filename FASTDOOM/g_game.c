7cc5bfb8032604bbd51bccffeb769ccbdc354af2
deat <deanradmad@gmail.com>
2022-03-27 21:26:43 +1100

    Restore "move forward" mouse button functionality

--- FASTDOOM/g_game.c
+++ FASTDOOM/g_game.c
@@ -186,7 +186,7 @@ void G_BuildTiccmd(ticcmd_t *cmd)
 
     forward = 0;
 
-    if (gamekeydown[key_up])
+    if (gamekeydown[key_up] || mousebuttons[mousebforward])
     {
         forward += forwardmove[speed];
     }
