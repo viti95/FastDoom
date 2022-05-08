f0fac449bee53d343fc253c27da7ca3a8fb31c81
viti95 <victornt95@gmail.com>
2022-05-09 01:17:54 +0200

    Reduce number of buttons that can be pressed

--- FASTDOOM/p_spec.h
+++ FASTDOOM/p_spec.h
@@ -184,8 +184,8 @@ typedef struct
 // max # of wall switches in a level
 #define MAXSWITCHES 50
 
-// 4 players, 4 buttons each at once, max.
-#define MAXBUTTONS 16
+// 1 player, 4 buttons each at once, max.
+#define MAXBUTTONS 4
 
 // 1 second, in ticks.
 #define BUTTONTIME 35
