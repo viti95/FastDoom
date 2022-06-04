1e6ad44c87c395006789a343b0b44939637178ed
viti95 <victornt95@gmail.com>
2022-06-04 13:28:16 +0200

    Add more cards to Hercules automap support

--- FASTDOOM/options.h
+++ FASTDOOM/options.h
@@ -1,7 +1,7 @@
 #ifndef __OPTIONS__
 #define __OPTIONS__
 
-#if defined(MODE_Y) || defined(MODE_VBE2_DIRECT) || defined(MODE_13H) || defined(MODE_VBE2) || defined(MODE_V2) || defined(MODE_CGA)
+#if defined(MODE_Y) || defined(MODE_VBE2_DIRECT) || defined(MODE_13H) || defined(MODE_VBE2) || defined(MODE_V2) || defined(MODE_CGA) || defined(MODE_EGA) || defined(MODE_EGA640) || defined(MODE_CVB) || defined(MODE_CGA_BW)
 #define SUPPORTS_HERCULES_AUTOMAP
 #endif
 
