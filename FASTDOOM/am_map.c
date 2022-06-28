2fb026d9b27166985b702deaaf25dd7d7e63002a
viti95 <victornt95@gmail.com>
2022-06-28 15:02:05 +0200

    Fix losing grid lines near the automap boundary

--- FASTDOOM/am_map.c
+++ FASTDOOM/am_map.c
@@ -1030,7 +1030,7 @@ void AM_drawGrid(int color)
 	// Figure out start of vertical gridlines
 	start = m_x;
 	if ((start - bmaporgx) % (MAPBLOCKUNITS << FRACBITS))
-		start += (MAPBLOCKUNITS << FRACBITS) - ((start - bmaporgx) % (MAPBLOCKUNITS << FRACBITS));
+		start -= ((start - bmaporgx) % (MAPBLOCKUNITS << FRACBITS));
 	end = m_x + m_w;
 
 	// draw vertical gridlines
@@ -1046,7 +1046,7 @@ void AM_drawGrid(int color)
 	// Figure out start of horizontal gridlines
 	start = m_y;
 	if ((start - bmaporgy) % (MAPBLOCKUNITS << FRACBITS))
-		start += (MAPBLOCKUNITS << FRACBITS) - ((start - bmaporgy) % (MAPBLOCKUNITS << FRACBITS));
+		start -= ((start - bmaporgy) % (MAPBLOCKUNITS << FRACBITS));
 	end = m_y + m_h;
 
 	// draw horizontal gridlines
