8912a6db6057083697fb1b019b9deaf7dba6ab04
viti95 <victornt95@gmail.com>
2022-10-16 23:11:38 +0200

    Enable hercules automap on EW1

--- FASTDOOM/options.h
+++ FASTDOOM/options.h
@@ -1,7 +1,7 @@
 #ifndef __OPTIONS__
 #define __OPTIONS__
 
-#if defined(MODE_Y) || defined(MODE_VBE2_DIRECT) || defined(MODE_13H) || defined(MODE_VBE2) || defined(MODE_V2) || defined(MODE_CGA) || defined(MODE_EGA) || defined(MODE_EGA640) || defined(MODE_CVB) || defined(MODE_CGA_BW) || defined(MODE_PCP) || defined(MODE_EGA80)
+#if defined(MODE_Y) || defined(MODE_VBE2_DIRECT) || defined(MODE_13H) || defined(MODE_VBE2) || defined(MODE_V2) || defined(MODE_CGA) || defined(MODE_EGA) || defined(MODE_EGA640) || defined(MODE_CVB) || defined(MODE_CGA_BW) || defined(MODE_PCP) || defined(MODE_EGA80) || defined(MODE_EGAW1)
 #define SUPPORTS_HERCULES_AUTOMAP
 #endif
 
