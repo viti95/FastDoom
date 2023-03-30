f940a122e236aafa5d07754821f863a0b8ce605a
viti95 <victornt95@gmail.com>
2023-03-30 17:36:05 +0200

    R_DrawSpanLowBackbuffer

--- FASTDOOM/linearl.asm
+++ FASTDOOM/linearl.asm
@@ -139,7 +139,7 @@ align 4
 
 mapcalls:
   %assign LINE 0
-  %rep SCREENWIDTH+1
+  %rep SCREENWIDTH/2+1
     MAPDEFINE LINE
     %assign LINE LINE+1
   %endrep
@@ -193,29 +193,29 @@ CODE_SYM_DEF R_DrawSpanLowBackbuffer
 
 %assign LINE 0
 %assign PCOL 0
-%rep SCREENWIDTH/4
+%rep SCREENWIDTH/2
   %assign PLANE 0
-  %rep 4
     MAPLABEL LINE:
       %assign LINE LINE+1
-      %if LINE = 320
+      %if LINE = 160
         mov   al,[esi+ebx]           ; get source pixel
         mov   al,[eax]               ; translate color
-        mov   [edi+PLANE+PCOL*4],al  ; write pixel
+        mov   [edi+PLANE+PCOL*2],al  ; write pixel
+        mov   [edi+PLANE+PCOL*2+1],al  ; write pixel
       %else
         mov   al,[esi+ebx]           ; get source pixel
         shld  ebx,ecx,22             ; shift y units in
         mov   al,[eax]               ; translate color
         shld  ebx,ecx,6              ; shift x units in
-        mov   [edi+PLANE+PCOL*4],al  ; write pixel
+        mov   [edi+PLANE+PCOL*2],al  ; write pixel
+        mov   [edi+PLANE+PCOL*2+1],al  ; write pixel
         and   ebx,ebp                ; mask off slop bits
         add   ecx,edx                ; position += step
       %endif
       %assign PLANE PLANE+1
-  %endrep
 %assign PCOL PCOL+1
 %endrep
 
-hmap320: ret
+hmap160: ret
 
 %endif
