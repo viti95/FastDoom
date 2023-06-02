5f0533770f6e14810087b11451579849a8b04095
viti95 <victornt95@gmail.com>
2023-06-02 12:54:36 +0200

    ASM version for R_DrawFuzzColumnVBE2

---
+++ FASTDOOM/vbe2dh2.asm
@@ -0,0 +1,171 @@
+;
+; DESCRIPTION: Assembly texture mapping routines for VESA modes
+;
+
+BITS 32
+%include "macros.inc"
+
+%ifdef MODE_VBE2_DIRECT
+%include "defs.inc"
+
+extern _destview
+extern _viewheight
+extern _fuzzoffset
+extern _fuzzpos
+extern _colormaps
+
+;============================================================================
+; unwound vertical scaling code
+;
+; eax   light table pointer, 0 lowbyte overwritten
+; ebx   all 0, low byte overwritten
+; ecx   fractional step value
+; edx   fractional scale value
+; esi   start of source pixels
+; edi   bottom pixel in screenbuffer to blit into
+;
+; ebx should be set to 0 0 0 dh to feed the pipeline
+;
+; The graphics wrap vertically at 128 pixels
+;============================================================================
+
+BEGIN_DATA_SECTION
+
+%macro SCALEDEFINE 1
+  dd vscale%1
+%endmacro
+
+%macro TESTFUZZPOSDEFINE 1
+  dd testfuzzpos%1
+%endmacro
+
+align 4
+
+scalecalls:
+  %assign LINE 0
+  %rep SCREENHEIGHT+1
+    SCALEDEFINE LINE
+  %assign LINE LINE+1
+  %endrep
+
+;============================================================================
+
+BEGIN_CODE_SECTION
+
+;============================================================================
+;
+; R_DrawColumn
+;
+;============================================================================
+
+CODE_SYM_DEF R_DrawFuzzColumnVBE2
+	push		edi
+	push		ebx
+	push		ecx
+	push		edx
+	push		esi
+	push		ebp
+
+  mov  eax,[_viewheight]
+  dec  eax
+
+  mov  ebp,[_dc_yh]
+
+  cmp  eax,ebp
+  jne  dc_yhOK
+
+  dec  eax
+  mov  ebp,eax
+
+dc_yhOK:
+  mov  eax,[_dc_yl]
+
+  test eax,eax
+  jne dc_ylOK
+
+  mov  eax,1
+
+dc_ylOK:
+  lea  edi,[ebp+ebp*4]
+  sub  ebp,eax ; ebp = pixel count
+  js   near .done
+
+  shl  edi,6
+  add  edi,[_dc_x]
+  add  edi,[_destview]
+
+  mov esi,[_colormaps]
+  mov	ecx,[_fuzzpos]
+  add esi,0x600
+  mov edx,_fuzzoffset
+  mov ebx,0x32
+
+  jmp  [scalecalls+4+ebp*4]
+
+.done:
+	pop		ebp
+	pop		esi
+	pop		edx
+	pop		ecx
+	pop		ebx
+  pop		edi
+  ret
+; R_DrawColumnVBE2 ends
+
+;============ HIGH DETAIL ============
+
+%macro SCALELABEL 1
+  vscale%1
+%endmacro
+
+%macro TESTFUZZPOSDEFINE 1
+  testfuzzpos%1
+%endmacro
+
+%macro JMPTESTFUZZPOSDEFINE 1
+  jne testfuzzpos%1
+%endmacro
+
+%assign LINE SCREENHEIGHT
+%rep SCREENHEIGHT-1
+  SCALELABEL LINE:
+
+  mov		ebp,[edx+ecx*4]
+  inc   ecx
+	mov   al,[ebp+edi-(LINE-1)*320]
+  cmp   cl,bl
+	mov		al,[eax+esi]
+  mov		[edi-(LINE-1)*320],al
+
+  JMPTESTFUZZPOSDEFINE LINE
+  xor   ecx,ecx
+
+  TESTFUZZPOSDEFINE LINE:
+  %assign LINE LINE-1
+%endrep
+
+vscale1:
+
+  mov		ebp,[edx+ecx*4]
+	inc   ecx
+  mov   al,[ebp+edi-(LINE-1)*320]
+  cmp   cl,bl
+	mov		al,[eax+esi]
+  mov		[edi-(LINE-1)*320],al
+  
+  jne   testfuzzpos1
+  xor   ecx,ecx
+
+testfuzzpos1:
+  mov [_fuzzpos],ecx
+	pop	ebp
+  pop	esi
+  pop	edx
+
+vscale0:
+	pop	ecx
+	pop	ebx
+  pop	edi
+  ret
+
+%endif
