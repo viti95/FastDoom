17115220b6885cb577d66d387265e6f489e70137
viti95 <victornt95@gmail.com>
2023-07-19 10:38:39 +0200

    Force faster cpu speed for stub

--- stub.sh
+++ stub.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-dosbox-x -fastlaunch -nogui -noautoexec -noconfig -c "mount J ." -c "J:" -c "SET DOS32A=J:\DOS32A" -c "SET PATH=%PATH%;J:\DOS32A\BINW" -c "stub2.bat" &>/dev/null
+dosbox-x -fastlaunch -nomenu -nogui -noautoexec -noconfig -c "config -set cycles=max" -c "mount J ." -c "J:" -c "SET DOS32A=J:\DOS32A" -c "SET PATH=%PATH%;J:\DOS32A\BINW" -c "stub2.bat" &>/dev/null
