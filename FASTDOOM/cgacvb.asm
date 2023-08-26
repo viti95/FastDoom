e64d7be234f0be040924df8b5423b68b3ed764f7
viti95 <victornt95@gmail.com>
2023-08-26 14:34:24 +0200

    Free EAX register

--- FASTDOOM/cgacvb.asm
+++ FASTDOOM/cgacvb.asm
@@ -31,74 +31,69 @@ _vrambuffer: times 16384 db 0
 BEGIN_CODE_SECTION
 
 CODE_SYM_DEF I_FinishUpdate
-	push		ebx
-	push		ecx
-	push		edx
-	push		esi
-	push		edi
-	push		ebp
+	push	ebx
+	push	ecx
+	push	edx
+	push	esi
+	push	edi
+	push	ebp
 	sub		esp,0x14
-	mov		edx,0xB8000
-	mov		eax,_vrambuffer
+
+	xor		edx,edx
 	xor		ebx,ebx
 L$2:
 	xor		ch,ch
 L$3:
-	movzx		esi,bx
+	movzx	esi,bx
 	mov		ebp,[_ptrlut16colors]
-	movzx		edi,byte _backbuffer[esi]
+	movzx	edi,byte _backbuffer[esi]
 	mov		cl,[edi+ebp]
-	movzx		esi,byte _backbuffer+0x2[esi]
+	movzx	esi,byte _backbuffer+0x2[esi]
 	mov		0x10[esp],cl
 	mov		cl,[esi+ebp]
 	mov		0x8[esp],cl
 	mov		cl,0x10[esp]
 	shl		cl,0x4
 	or		cl,0x8[esp]
-	cmp		cl,[eax]
+	cmp		cl,[_vrambuffer+edx]
 	je		L$4
 L$7:
-	mov		[edx],cl
-	mov		[eax],cl
+	mov		[0xB8000+edx],cl
+	mov		[_vrambuffer+edx],cl
 L$4:
 	inc		ch
 	add		ebx,0x4
 	inc		edx
-	inc		eax
 	cmp		ch,0x50
 	jb		L$3
 	add		edx,0x1FB0
-	add		eax,0x1FB0
 	xor		ch,ch
-	lea		eax,[eax]
 L$5:
-	movzx		esi,bx
-	movzx		edi,byte _backbuffer[esi]
+	movzx	esi,bx
+	movzx	edi,byte _backbuffer[esi]
 	mov		[esp],edi
 	mov		ebp,[esp]
 	mov		edi,[_ptrlut16colors]
 	add		ebp,edi
 	mov		cl,[ebp]
-	movzx		esi,byte _backbuffer+0x2[esi]
+	movzx	esi,byte _backbuffer+0x2[esi]
 	mov		0xc[esp],cl
 	mov		cl,[edi+esi]
 	mov		0x4[esp],cl
 	mov		cl,0xc[esp]
 	shl		cl,0x4
 	or		cl,0x4[esp]
-	cmp		cl,[eax]
+	cmp		cl,[_vrambuffer+edx]
 	je		L$6
-	mov		[edx],cl
-	mov		[eax],cl
+	mov		[0xB8000+edx],cl
+	mov		[_vrambuffer+edx],cl
 L$6:
 	inc		ch
 	add		ebx,0x4
 	inc		edx
-	inc		eax
 	cmp		ch,0x50
 	jb		L$5
 	sub		edx,0x2000
-	sub		eax,0x2000
 	cmp		bx,0xFA00
 	jb		L$2
 	add		esp,0x14
