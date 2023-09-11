482c99613b944cb8a7278e58fc66ab88e905f795
viti95 <victornt95@gmail.com>
2023-09-11 09:59:27 +0200

    New linux script to build release packages directly. Also create a EXE folder (prebuilt executables). No more copy&paste errors!

---
+++ package.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+./buildall.sh
+
+rm -rf PKG
+
+mkdir PKG
+
+cp *.TCF PKG/
+cp FDOOM*.EXE PKG/
+cp WAD/*.WAD PKG/
+cp README.txt PKG/
+cp synthgs.sbk PKG/
+cp BENCH.BAT PKG/
+cp -r INTER PKG/INTER
+cp -r BENCH PKG/BENCH
+cp EXE/* PKG
+
+versionstring=$(grep "#define FDOOMVERSION" FASTDOOM/version.h)
+version=$(echo "$versionstring" | awk -F'"' '{print $2}')
+
+rm FastDoom_$version.zip
+
+cd PKG
+zip -r -9 ../FastDoom_$version.zip .
+cd ..
