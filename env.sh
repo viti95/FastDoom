a64ca32571f8424122299f31ea59f6480a7999a0
viti95 <victornt95@gmail.com>
2024-02-26 12:52:17 +0100

    Fix env.sh when Watcom is not installed

--- env.sh
+++ env.sh
@@ -1,16 +1,23 @@
+final_dir=""
+
 for dir in \
         "/usr/bin/watcom" \
         "/opt/watcom"
 do
     if [ -d "$dir" ]; then
-        export WATCOM=$dir
+        final_dir=$dir
         break
     fi
 done
 
-export PATH=$WATCOM/binl:$PATH
-export EDPATH=$WATCOM/eddat
-export WIPFC=$WATCOM/wipfc
-export INCLUDE=$WATCOM/h
+if [[ -z "$final_dir" ]]; then
+    echo "Watcom install not found."
+else
+    export WATCOM=$final_dir
+    export PATH=$WATCOM/binl:$PATH
+    export EDPATH=$WATCOM/eddat
+    export WIPFC=$WATCOM/wipfc
+    export INCLUDE=$WATCOM/h
 
-echo -e "Watcom folder: $dir"
+    echo -e "Using Watcom on $final_dir"
+fi
