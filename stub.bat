329d35585731d18983752683c605378479278836
viti95 <victornt95@gmail.com>
2024-06-11 16:34:45 +0200

    Better stub scripts. Now DosBox-X doesn't create a window, and every step done is shown in the console.

--- stub.bat
+++ stub.bat
@@ -2,17 +2,17 @@
 
 if "%1"=="" goto all
 
-sb /R %1
-ss %1 dos32a.d32
+sb /R %1 >> stub.log
+ss %1 dos32a.d32 >> stub.log
 
 exit
 
 :all
 
-for %%f in (fdoom*.exe) do sb /R %%f
-for %%f in (fdoom*.exe) do ss %%f dos32a.d32
-for %%f in (fdm*.exe) do sb /R %%f
-for %%f in (fdm*.exe) do ss %%f dos32a.d32
+for %%f in (fdoom*.exe) do sb /R %%f >> stub.log
+for %%f in (fdoom*.exe) do ss %%f dos32a.d32 >> stub.log
+for %%f in (fdm*.exe) do sb /R %%f >> stub.log
+for %%f in (fdm*.exe) do ss %%f dos32a.d32 >> stub.log
 
 exit
 
--- stub.sh
+++ stub.sh
@@ -14,10 +14,16 @@ fi
 
 echo "FastDoom DOS32/A Stub script"
 
+echo -n > STUB.LOG
+tail -F STUB.LOG &
+tail_pid=$!
+
 # Try native apps
 if type dosbox-x &>/dev/null; then
   echo "Using native DOSBox-X"
-  dosbox-x -fastlaunch -nomenu -nogui -noautoexec -noconfig -c "config -set cycles=max" -c "mount J ." -c "J:" -c "SET DOS32A=J:\DOS32A" -c "SET PATH=%PATH%;J:\DOS32A\BINW" -c "$parameter" &>/dev/null
+  dosbox-x -fastlaunch -silent -nomenu -nogui -noautoexec -noconfig -c "config -set cycles=max" -c "mount J ." -c "J:" -c "SET DOS32A=J:\DOS32A" -c "SET PATH=%PATH%;J:\DOS32A\BINW" -c "$parameter" &>/dev/null
+  kill -TERM $tail_pid
+  rm STUB.LOG
   echo "Done"
   exit
 fi
@@ -27,7 +33,9 @@ echo "Native DOSBox-X not found"
 # Try flatpak app
 if flatpak info com.dosbox_x.DOSBox-X > /dev/null 2>&1; then
     echo "Using Flatpak DOSBox-X"
-    flatpak run com.dosbox_x.DOSBox-X -fastlaunch -nomenu -nogui -noautoexec -noconfig -c "config -set cycles=max" -c "mount J ." -c "J:" -c "SET DOS32A=J:\DOS32A" -c "SET PATH=%PATH%;J:\DOS32A\BINW" -c "$parameter" &>/dev/null
+    flatpak run com.dosbox_x.DOSBox-X -fastlaunch -silent -nomenu -nogui -noautoexec -noconfig -c "config -set cycles=max" -c "mount J ." -c "J:" -c "SET DOS32A=J:\DOS32A" -c "SET PATH=%PATH%;J:\DOS32A\BINW" -c "$parameter" &>/dev/null
+    kill -TERM $tail_pid
+    rm STUB.LOG
     echo "Done"
     exit
 fi
