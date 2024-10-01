c34d496b82f2412fb4ddcd93ad04e21adb640088
viti95 <victornt95@gmail.com>
2024-10-01 21:57:28 +0200

    Optimize backbuffer high 1:1

--- FASTDOOM/linearh5.asm
+++ FASTDOOM/linearh5.asm
@@ -81,9 +81,13 @@ patchCentery:
   sub  eax,0x12345678
   mov  esi,[_dc_source]
   mov  ebx,[_dc_x]
-  lea  esi,[esi+eax+0x64]
+
+  lea  esi,[esi+eax+0x64-(SCREENHEIGHT/2)]
+
   add  edi,[_columnofs+ebx*4]
   mov  eax,[_dc_colormap]
+
+  add  esi,ebp
 
   jmp  [scalecalls+4+ebp*4]
 
@@ -115,6 +119,8 @@ CODE_SYM_DEF R_DrawColumnBackbufferDirect
   add  edi,[_columnofs+ebx*4]
   mov  eax,[_dc_colormap]
 
+  lea  esi,[esi+ebp-(SCREENHEIGHT/2)]
+
   jmp  [scalecalls+4+ebp*4]
 
 doneh:
@@ -132,18 +138,19 @@ doneh:
 %endmacro
 
 %assign LINE SCREENHEIGHT
+%assign POSITION -(SCREENHEIGHT / 2)
 %rep SCREENHEIGHT-1
   SCALELABEL LINE:
-    mov  al,[esi]                   ; get source pixel
+    mov  al,[esi+POSITION]              ; get source pixel
     mov  al,[eax]                       ; translate the color
-    inc  esi
     mov  [edi-(LINE-1)*SCREENWIDTH],al  ; draw a pixel to the buffer
     %assign LINE LINE-1
+    %assign POSITION POSITION+1
 %endrep
 
 vscale1:
   pop	ebp
-  mov al,[esi]
+  mov al,[esi+POSITION]
   pop	esi
   mov al,[eax]
   pop	edx
