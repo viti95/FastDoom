b1dce8d5425fd1a236d2190b8b5dfc48bc238174
viti95 <victornt95@gmail.com>
2024-11-16 02:26:44 +0100

    Script for Plutonia PCM music

---
+++ SCRIPTS/PCMconvert/convert_plutonia.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+sox d_runnin.flac -r 11025 -e unsigned -b 8 -c 1 MUS_1.RAW
+sox d_stalks.flac -r 11025 -e unsigned -b 8 -c 1 MUS_2.RAW
+sox d_countd.flac -r 11025 -e unsigned -b 8 -c 1 MUS_3.RAW
+sox d_betwee.flac -r 11025 -e unsigned -b 8 -c 1 MUS_4.RAW
+sox d_doom.flac -r 11025 -e unsigned -b 8 -c 1 MUS_5.RAW
+sox d_the_da.flac -r 11025 -e unsigned -b 8 -c 1 MUS_6.RAW
+sox d_shawn.flac -r 11025 -e unsigned -b 8 -c 1 MUS_7.RAW
+sox d_ddtblu.flac -r 11025 -e unsigned -b 8 -c 1 MUS_8.RAW
+sox d_in_cit.flac -r 11025 -e unsigned -b 8 -c 1 MUS_9.RAW
+sox d_dead.flac -r 11025 -e unsigned -b 8 -c 1 MUS_10.RAW
+sox d_stlks2.flac -r 11025 -e unsigned -b 8 -c 1 MUS_11.RAW
+sox d_theda2.flac -r 11025 -e unsigned -b 8 -c 1 MUS_12.RAW
+sox d_doom2.flac -r 11025 -e unsigned -b 8 -c 1 MUS_13.RAW
+sox d_ddtbl2.flac -r 11025 -e unsigned -b 8 -c 1 MUS_14.RAW
+sox d_runni2.flac -r 11025 -e unsigned -b 8 -c 1 MUS_15.RAW
+sox d_dead2.flac -r 11025 -e unsigned -b 8 -c 1 MUS_16.RAW
+sox d_stlks3.flac -r 11025 -e unsigned -b 8 -c 1 MUS_17.RAW
+sox d_shawn2.flac -r 11025 -e unsigned -b 8 -c 1 MUS_18.RAW
+sox d_messag.flac -r 11025 -e unsigned -b 8 -c 1 MUS_19.RAW
+sox d_count2.flac -r 11025 -e unsigned -b 8 -c 1 MUS_20.RAW
+sox d_ddtbl3.flac -r 11025 -e unsigned -b 8 -c 1 MUS_21.RAW
+sox d_ampie.flac -r 11025 -e unsigned -b 8 -c 1 MUS_22.RAW
+sox d_theda3.flac -r 11025 -e unsigned -b 8 -c 1 MUS_23.RAW
+sox d_adrian.flac -r 11025 -e unsigned -b 8 -c 1 MUS_24.RAW
+sox d_openin.flac -r 11025 -e unsigned -b 8 -c 1 MUS_25.RAW
+sox d_dm2ttl.flac -r 11025 -e unsigned -b 8 -c 1 MUS_26.RAW
+sox d_dm2int.flac -r 11025 -e unsigned -b 8 -c 1 MUS_27.RAW
