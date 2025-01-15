ac2b782e8dad23445d6a3ceea9e9b2bdf9c3c1eb
viti95 <victornt95@gmail.com>
2025-01-15 16:39:19 +0100

    Use Watcom 64-bit executables for X86_64

--- fdenv.sh
+++ fdenv.sh
@@ -18,7 +18,11 @@ else
     arch=$(uname -m)
 
     if [ "$arch" == "aarch64" ]; then
+        echo "ARM64 architecture"
         export PATH=$WATCOM/arml64:$PATH
+    elif [ "$arch" == "x86_64" ]; then
+        echo "X86_64 architecture"
+        export PATH=$WATCOM/binl64:$PATH
     else
         export PATH=$WATCOM/binl:$PATH
     fi
