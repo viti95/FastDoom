52652a08a9bbf7dc391aaeb82f3c9cf15b7f89fe
viti95 <victornt95@gmail.com>
2025-03-11 20:06:00 +0100

    Output more information in disassemblied objects

--- disasm.sh
+++ disasm.sh
@@ -9,7 +9,7 @@ cd DISASM
 for f in ../FASTDOOM/*.obj; do
     filename=$(basename "$f" .obj)
     #echo $filename.obj
-    wdis -l="$filename.lst" ../FASTDOOM/"$filename".obj
+    wdis -l="$filename.lst" -e -p ../FASTDOOM/"$filename".obj
 done
 
 cd ..
