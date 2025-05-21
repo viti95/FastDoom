4756f3f55d369d0a5090de5b74cdac6f0e635af1
ViTi95 <victornt95@gmail.com>
2025-05-21 22:43:01 +0200

    Add dosemu2 basic support

---
+++ confDOS.bat
@@ -0,0 +1,7 @@
+@echo off
+
+SET DOS32A=F:\DOS32A
+SET PATH=%PATH%;F:\DOS32A\BINW
+
+stubdbox.bat
+exitemu
--- stub.sh
+++ stub.sh
@@ -18,29 +18,29 @@ echo -n > STUB.LOG
 tail -F STUB.LOG &
 tail_pid=$!
 
-# Try native apps
+echo "Trying native DOSBox-X..."
+
 if type dosbox-x &>/dev/null; then
   echo "Using native DOSBox-X"
   dosbox-x -fastlaunch -silent -nomenu -nogui -noautoexec -noconfig -c "config -set cycles=max" -c "mount J ." -c "J:" -c "SET DOS32A=J:\DOS32A" -c "SET PATH=%PATH%;J:\DOS32A\BINW" -c "$parameter" &>/dev/null
   kill -TERM $tail_pid
-  rm STUB.LOG
+  rm -f STUB.LOG
   echo "Done"
   exit
 fi
 
-echo "Native DOSBox-X not found"
+echo "Trying Flatpak DOSBox-X..."
 
-# Try flatpak app
 if flatpak info com.dosbox_x.DOSBox-X > /dev/null 2>&1; then
     echo "Using Flatpak DOSBox-X"
     flatpak run com.dosbox_x.DOSBox-X -fastlaunch -silent -nomenu -nogui -noautoexec -noconfig -c "config -set cycles=max" -c "mount J ." -c "J:" -c "SET DOS32A=J:\DOS32A" -c "SET PATH=%PATH%;J:\DOS32A\BINW" -c "$parameter" &>/dev/null
     kill -TERM $tail_pid
-    rm STUB.LOG
+    rm -f STUB.LOG
     echo "Done"
     exit
 fi
 
-echo "Flatpak DOSBox-X not found"
+echo "Trying native DOSBox..."
 
 if type dosbox &>/dev/null; then
   echo "Using native DOSBox (classic)"
@@ -52,13 +52,35 @@ if type dosbox &>/dev/null; then
       echo "ss $f dos32a.d32 >> stub.log" >> stubdbox.bat
   done
   echo "exit" >> stubdbox.bat
-  cat stubdbox.bat 
+  #cat stubdbox.bat 
   SDL_VIDEODRIVER=dummy dosbox  -exit -c "config -set cycles=max" -c "mount J ." -c "J:" -c "SET DOS32A=J:\DOS32A" -c "SET PATH=%PATH%;J:\DOS32A\BINW" -c "stubdbox.bat" &>/dev/null
   rm -f stubdbox.bat
   kill -TERM $tail_pid
-  rm STUB.LOG
+  rm -f STUB.LOG
   echo "Done"
   exit
 fi
 
-echo "No suitable DOSBox-X installation found. Abort"
+echo "Trying native DOSEMU2..."
+
+if type dosemu &>/dev/null; then
+  echo "Using native DOSEMU2"
+  # NOTE: dosemu2 does not support the advanced for included in stub.bat
+  echo "@echo off" > stubdbox.bat 
+  for f in FDOOM*.EXE FDM*.EXE; do
+      [ -e "$f" ] || continue
+      echo "sb /R $f >> stub.log" >> stubdbox.bat
+      echo "ss $f dos32a.d32 >> stub.log" >> stubdbox.bat
+  done
+  #cat stubdbox.bat 
+  dosemu -K . -n -t -E confDOS.bat > file 2>&1
+  # NOTE: dosemu2 doesn't create files in uppercase
+  for file in *.exe; do mv -- "$file" "${file^^}"; done
+  rm -f stubdbox.bat
+  kill -TERM $tail_pid
+  rm -f STUB.LOG
+  echo "Done"
+  exit
+fi
+
+echo "No suitable DOS emulator found. Abort"
