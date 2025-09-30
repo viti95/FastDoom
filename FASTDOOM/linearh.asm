14b2e1ba2bfc08c3f57b52691ca5cbc15c0fd99b
viti95 <victornt95@gmail.com>
2025-09-30 16:09:17 +0200

    Test span renderer variant for Pentium processors. SHLD is slow and doesn’t pair, so this version avoids those instructions and uses ones that pair well

--- FASTDOOM/linearh.asm
+++ FASTDOOM/linearh.asm
@@ -194,7 +194,7 @@ CODE_SYM_DEF R_DrawSpanBackbuffer
   mov     eax,[mapcalls+eax*4]
   mov     ecx,[_ds_frac]        ; build composite position
   mov     [callpoint],eax       ; spot to jump into unwound
-  mov     edx,[_ds_step]        ; build composite step
+  mov     ebp,[_ds_step]        ; build composite step
   mov     eax,[mapcalls+4+ebx*4]
   mov     esi,[_ds_source]
   mov     [returnpoint],eax     ; spot to patch a ret at
@@ -209,11 +209,15 @@ patchColumnofs:
 
   ; feed the pipeline and jump in
 
-  shld    ebx,ecx,22  ; shift y units in
-  mov     ebp,0x0FFF  ; used to mask off slop high bits from position
-  shld    ebx,ecx,6   ; shift x units in
-  and     ebx,ebp     ; mask off slop bits
-  add     ecx,edx
+  mov   ebx,ecx
+  mov   edx,ecx
+  shr   ebx,10
+  shr   edx,26
+  shl   ebx,6
+  add   ecx,ebp
+  or    ebx,edx
+  and   ebx,0x0FFF
+
   call    [callpoint]
 
   mov     ebx,[returnpoint]
@@ -243,13 +247,25 @@ patchColumnofs:
         mov   al,[eax]               ; translate color
         mov   [edi+PLANE+PCOL],al  ; write pixel
       %else
-        mov   al,[esi+ebx]           ; get source pixel
-        shld  ebx,ecx,22             ; shift y units in
-        mov   al,[eax]               ; translate color
-        shld  ebx,ecx,6              ; shift x units in
-        mov   [edi+PLANE+PCOL],al  ; write pixel
-        and   ebx,ebp                ; mask off slop bits
-        add   ecx,edx                ; position += step
+        ;mov   al,[esi+ebx]           ; get source pixel
+        ;shld  ebx,ecx,22         ebx    ; shift y units in
+        ;mov   al,[eax]               ; translate color
+        ;shld  ebx,ecx,6              ; shift x units in
+        ;mov   [edi+PLANE+PCOL],al  ; write pixel
+        ;and   ebx,ebp                ; mask off slop bits
+        ;add   ecx,edx                ; position += step
+
+        mov   al,[esi+ebx]
+        mov   ebx,ecx
+        mov   edx,ecx
+        shr   ebx,10
+        mov   al,[eax]
+        shr   edx,26
+        mov   [edi+PLANE+PCOL],al
+        shl   ebx,6
+        add   ecx,ebp
+        or    ebx,edx
+        and   ebx,0x0FFF
       %endif
       %assign PLANE PLANE+1
 %assign PCOL PCOL+1
