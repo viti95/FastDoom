eea27f8de53ea4398840c55a1d804e09081bf775
viti95 <victornt95@gmail.com>
2025-12-04 23:02:47 +0100

    Initial version of MMX span rendering for VBE2 direct modes

--- FASTDOOM/r_main.c
+++ FASTDOOM/r_main.c
@@ -2519,10 +2519,12 @@ void R_ExecuteSetViewSize(void)
             switch (selectedCPU)
             {
             case INTEL_PENTIUM:
-            case INTEL_PENTIUM_MMX:
             case IDT_WINCHIP:
             case CYRIX_6X86:
                 spanfunc = R_DrawSpanVBE2Pentium;
+                break;
+            case INTEL_PENTIUM_MMX:
+                spanfunc = R_DrawSpanVBE2MMX;
                 break;
             default:
                 spanfunc = R_DrawSpanVBE2;
