0bcc08ad30a425b7429fa163c92245a6e4a919bb
ViTi95 <victornt95@gmail.com>
2025-12-07 18:15:41 +0100

    Also a bit faster on the Winchip

--- FASTDOOM/r_main.c
+++ FASTDOOM/r_main.c
@@ -2551,6 +2551,7 @@ void R_ExecuteSetViewSize(void)
             case CYRIX_6X86:
                 spanfunc = R_DrawSpanVBE2Pentium;
                 break;
+            case IDT_WINCHIP:
             case CYRIX_6X86MX:
             case RISE_MP6:
             case INTEL_PENTIUM_MMX:
